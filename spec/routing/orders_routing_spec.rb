0d0037c8c5f445af0aea5f969cb4c52a17490856
Quan Vu <nongdenchet8435848@gmail.com>
2016-03-28 00:02:52 +0700

    small fix ui

--- spec/routing/orders_routing_spec.rb
+++ spec/routing/orders_routing_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id          :integer          not null, primary key
+#  total_money :decimal(, )
+#  username    :string
+#  email       :string
+#  phone       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe OrdersController, type: :routing do
